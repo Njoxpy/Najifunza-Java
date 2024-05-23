7af4ecfb3777230a3347a576ab85bfc131b60ce5
Godbless Nyagawa <110627172+Njoxpy@users.noreply.github.com>
2024-05-23 18:48:38 +0300

    added files for Java
    Done.

---
+++ src/CodesCracker3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class CodesCracker3
+{
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        n = s.nextInt();
+
+        for(int i=0; i<n; i++)
+            System.out.println("Hello, World");
+    }
+}
